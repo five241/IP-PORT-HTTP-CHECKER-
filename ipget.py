6f8f6cfd13df476a87ca7b116547ef0e900a46cd
MR 7UM4D1 <38749430+five241@users.noreply.github.com>
2024-05-24 18:16:06 +0700

    Update ipget.py

--- ipget.py
+++ ipget.py
@@ -1,3 +1,25 @@
+python_banner = '''
+8888888 8888888b.        .d8888b.           888    
+  888   888   Y88b      d88P  Y88b          888    
+  888   888    888      888    888          888    
+  888   888   d88P      888         .d88b.  888888 
+  888   8888888P"       888  88888 d8P  Y8b 888    
+  888   888             888    888 88888888 888    
+  888   888             Y88b  d88P Y8b.     Y88b.  
+8888888 888              "Y8888P88  "Y8888   "Y888 
+        
+        BY 7UM4D1007
+        Tiktok:@7um4d1007
+        Telegram:@jumadi007
+'''
+
+# Menampilkan banner Python
+print(python_banner)
+import socket
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
 import random
 
 # Batas bawah dan atas rentang IP
